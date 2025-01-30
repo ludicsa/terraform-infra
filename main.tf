33d063160a3d6f7c1439a606daaf835ef8e28f98
ludi1999 <ludicsalucas1@gmail.com>
2025-01-30 19:48:29 -0300

    fourth commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+variable "lambda_runtime" {}
+variable "lambda_zip_file" {}
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "my_lambda_function"
+  runtime       = var.lambda_runtime
+  handler       = "index.handler"
+  filename      = var.lambda_zip_file
+  role          = aws_iam_role.lambda_role.arn
+}
