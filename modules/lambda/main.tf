bbe30c9c253fa5a4b62cabb61af853cd3a2f8194
ludicsa <lucas.ludicsa99@gmail.com>
2025-01-30 19:15:17 -0300

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "lambda_function" {
   function_name = "hello-world-${var.tech}-${var.environment}"
   role          = aws_iam_role.lambda_exec.arn
   handler       = "main.lambda_handler"
-  runtime       = var.runtime
+  runtime       = var.runtime  
 
   filename         = var.lambda_zip_path
   source_code_hash = filebase64sha256(var.lambda_zip_path)
