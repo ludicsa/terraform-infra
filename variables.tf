da75342bf07a93a079972f56f73d50e6083b0917
ludicsa <lucas.ludicsa99@gmail.com>
2025-01-30 18:47:37 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,1 @@
-variable "environment" {
-  description = "Ambiente de deploy (dev/prod)"
-  type        = string
-}
 
-variable "lambda_zip_path" {
-  description = "Caminho do arquivo ZIP da Lambda"
-  type        = string
-}
