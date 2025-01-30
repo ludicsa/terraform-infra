5567dab1b696b827edc00764bb77fb3690e0d849
ludicsa <lucas.ludicsa99@gmail.com>
2025-01-30 20:14:45 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,35 @@
 variable "lambda_runtime" {}
 variable "lambda_zip_file" {}
 
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy" {
+  name       = "lambda_basic_execution"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "lambda_function" {
   function_name = "my_lambda_function"
   runtime       = var.lambda_runtime
   handler       = "index.handler"
   filename      = var.lambda_zip_file
+  role          = aws_iam_role.lambda_role.arn  
 }
 
 
