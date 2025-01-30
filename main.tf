ea810d9ddf94f00a96c39d60abad3c20c8785cf1
ludicsa <lucas.ludicsa99@gmail.com>
2025-01-30 20:20:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "lambda_runtime" {}
 variable "lambda_zip_file" {}
 
 
-
+###LAMBDA
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_execution_role"
 
@@ -32,7 +32,40 @@ resource "aws_lambda_function" "lambda_function" {
   role          = aws_iam_role.lambda_role.arn  
 }
 
+###API-GATEWAY
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "lambda-api"
+  protocol_type = "HTTP"
+}
 
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.lambda_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.lambda_function.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.lambda_api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = "default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+}
+
+###BACKEND
 terraform {
   backend "s3" {
     bucket         = "terraform-tfstate-87879564"
