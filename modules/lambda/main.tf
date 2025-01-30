12a6420c88fa0835c2c05064ba0dd9a7b0bbda1c
ludicsa <lucas.ludicsa99@gmail.com>
2025-01-30 18:55:48 -0300

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -27,11 +27,11 @@ resource "aws_iam_policy_attachment" "lambda_logs" {
 
 # Criar a AWS Lambda
 resource "aws_lambda_function" "lambda_function" {
-  function_name = "hello-world-${var.environment}"
+  function_name = "hello-world-${var.tech}-${var.environment}"
   role          = aws_iam_role.lambda_exec.arn
   handler       = "main.lambda_handler"
   runtime       = "python3.9"
 
-  filename         = var.lambda_zip_path  # Arquivo ZIP gerado no pipeline
+  filename         = var.lambda_zip_path
   source_code_hash = filebase64sha256(var.lambda_zip_path)
 }
